ae8d1a41d860bcf8f5ddea163efacf41d7569cd6
Jozef Kutej <jozef@kutej.net>
2009-08-28 11:46:15 +0200

    slurp+spew with tests, Pod update

--- lib/IO/Any.pm
+++ lib/IO/Any.pm
@@ -6,34 +6,43 @@ IO::Any - open anything
 
 =head1 SYNOPSIS
 
+    # NOTE commented out lines doesn't work (jet)
     use IO::Any;
 
     $fh = IO::Any->read('filename');
     $fh = IO::Any->read('file://var/log/syslog');
-    $fh = IO::Any->read('http://search.cpan.org/');
-    $fh = IO::Any->read('-');
+    #$fh = IO::Any->read('http://search.cpan.org/');
+    #$fh = IO::Any->read('-');
     $fh = IO::Any->read(['folder', 'other-folder', 'filename']);
     $fh = IO::Any->read('folder');
     $fh = IO::Any->read("some text\nwith more lines\n");
     $fh = IO::Any->read(\"some text\nwith more lines\n");
     $fh = IO::Any->read('{"123":[1,2,3]}');
     $fh = IO::Any->read('<root><element>abc</element></root>');
-    $fh = IO::Any->read(IO::String->new("cba"));
-    $fh = IO::Any->read($object_with_toString_method);
+    #$fh = IO::Any->read(IO::String->new("cba"));
+    #$fh = IO::Any->read($object_with_toString_method);
 
     $fh = IO::Any->write('filename');
     $fh = IO::Any->write('file://var/log/syslog');
-    $fh = IO::Any->write('-');
+    #$fh = IO::Any->write('-');
     $fh = IO::Any->write(['folder', 'filename']);
-    $fh = IO::Any->write('=');
+    #$fh = IO::Any->write('=');
     my $string;
     $fh = IO::Any->write(\$string);
 
-=head1 NOTE
+    my $content = IO::Any->slurp(['folder', 'filename']);
+    IO::Any->spew(['folder2', 'filename'], $content);
 
-This is an experiment :-)
+    perl -MIO::Any -le 'print IO::Any->slurp("/etc/passwd")'
+    perl -MIO::Any -le 'IO::Any->spew("/tmp/timetick", time())'
 
 =head1 DESCRIPTION
+
+The aim is to provide read/write anything. The module tries to guess
+C<$what> the "anything" is based on some rules. See C</new> method Pod for
+examples and C</new> and C</_guess_what> code for the implementation.
+
+There are two methods C</slurp> and C</spew> to read/write whole C<$what>.
 
 =cut
 
@@ -47,11 +56,30 @@ use 5.010;
 use Carp 'croak';
 use Scalar::Util 'blessed';
 use IO::String;
+use IO::File;
 use File::Spec;
 
 =head1 METHODS
 
-=head2 new()
+=head2 new($what, $how)
+
+Open C<$what> in C<$how> mode.
+
+C<$what> can be:
+
+		'filename'                => [ 'file' => 'filename' ],
+		'folder/filename'         => [ 'file' => 'folder/filename' ],
+		'file:///folder/filename' => [ 'file' => '/folder/filename' ],
+		[ 'folder', 'filename' ]  => [ 'file' => File::Spec->catfile('folder', 'filename') ],
+		'http://a/b/c'            => [ 'http' => 'http://a/b/c' ],
+		'https://a/b/c'           => [ 'http' => 'https://a/b/c' ],
+		'{"123":[1,2,3]}'         => [ 'string' => '{"123":[1,2,3]}' ],
+		'[1,2,3]'                 => [ 'string' => '[1,2,3]' ],
+		'<xml></xml>'             => [ 'string' => '<xml></xml>' ],
+		"a\nb\nc\n"               => [ 'string' => "a\nb\nc\n" ],    
+
+Returns filehandle. L<IO::String> for 'string', L<IO::File> for 'file'.
+'http' not implemented jet :)
 
 =cut
 
@@ -66,7 +94,12 @@ sub new {
     
     given ($type) {
         when ('string') { return IO::String->new($proper_what) }
-        when ('file')   { open(my $fh, $how, $proper_what) or die $!; return $fh }
+        when ('file')   {
+            my $fh = IO::File->new;
+            $fh->open($proper_what, $how)
+                or die 'error opening file "'.$proper_what.'" - '.$!;
+            return $fh;
+        }
         when ('http')   { die 'no http support jet :-|' }
     }
 }
@@ -93,7 +126,8 @@ sub _guess_what {
     }
     
     given (ref $what) {
-        when ('ARRAY') { return ('file', File::Spec->catfile(@{$what})) }
+        when ('ARRAY')  { return ('file', File::Spec->catfile(@{$what})) }
+        when ('SCALAR') { return ('string', $what) }
         when ('')      {} # do nothing here if not reference
         default { croak 'no support for ref '.(ref $what) }
     }
@@ -109,6 +143,13 @@ sub _guess_what {
     }
 }
 
+
+=head2 read($what)
+
+Same as C<<IO::Any->new($what, '<');>> or C<<IO::Any->new($what);>>.
+
+=cut
+
 sub read {
     my $class = shift;
     my $what  = shift;
@@ -117,6 +158,14 @@ sub read {
     
     return $class->new($what, '<');
 }
+
+
+=head2 write($what)
+
+Same as C<<IO::Any->new($what, '>');>>
+
+=cut
+
 sub write {
     my $class = shift;
     my $what  = shift;
@@ -124,6 +173,41 @@ sub write {
         if @_;
     
     return $class->new($what, '>');
+}
+
+
+=head2 slurp($what)
+
+Returns content of C<$what>.
+
+=cut
+
+sub slurp {
+    my $class = shift;
+    my $what  = shift;
+    croak 'too many arguments'
+        if @_;
+    
+    my $fh = $class->read($what);
+    return do { local $/; <$fh> };
+}
+
+
+=head2 slurp($what, $data)
+
+Writes C<$data> to C<$what>.
+
+=cut
+
+sub spew {
+    my $class = shift;
+    my $what  = shift;
+    my $data  = shift;
+    croak 'too many arguments'
+        if @_;
+    
+    my $fh = $class->write($what);
+    return $fh->print($data);
 }
 
 1;
--- t/01_IO-Any.t
+++ t/01_IO-Any.t
@@ -3,10 +3,12 @@
 use strict;
 use warnings;
 
-use Test::More 'no_plan';
-#use Test::More tests => 10;
+#use Test::More 'no_plan';
+use Test::More tests => 17;
 use Test::Differences;
 use File::Spec;
+use File::Temp 'tempdir';
+use File::Slurp 'read_file';
 
 use FindBin qw($Bin);
 use lib "$Bin/lib";
@@ -18,6 +20,8 @@ BEGIN {
 exit main();
 
 sub main {
+	my $tmpdir = tempdir( CLEANUP => 1 );
+
 	my @riddles = (
 		'filename'                => [ 'file' => 'filename' ],
 		'folder/filename'         => [ 'file' => 'folder/filename' ],
@@ -34,6 +38,34 @@ sub main {
 	while (my ($question, $answer) = splice(@riddles,0,2)) {
 		eq_or_diff([ IO::Any->_guess_what($question) ], $answer, 'guess what is "'.$question.'"')
 	}
+	
+	isa_ok(IO::Any->read([$Bin, 'stock', '01.txt']), 'IO::File', 'IO::Any->read([])');
+	isa_ok(IO::Any->read('{}'), 'IO::String', 'IO::Any->read("{}")');
+	
+	eq_or_diff(
+		[ IO::Any->slurp([$Bin, 'stock', '01.txt']) ],
+		[ qq{1\n22\n333\n} ],
+		'[ IO::Any->slurp() ]'
+	);
+	eq_or_diff(
+		scalar IO::Any->slurp([$Bin, 'stock', '01.txt']),
+		qq{1\n22\n333\n},
+		'scalar IO::Any->slurp()'
+	);
+	
+	IO::Any->spew([$tmpdir, '01-test.txt'], qq{4\n55\n666\n});
+	eq_or_diff(
+		scalar read_file(File::Spec->catfile($tmpdir, '01-test.txt')),
+		qq{4\n55\n666\n},
+		'IO::Any->spew()'
+	);
+	my $str;
+	IO::Any->spew(\$str, qq{1\n22\n333\n});
+	eq_or_diff(
+		$str,
+		qq{1\n22\n333\n},
+		'IO::Any->spew(\$str)'
+	);
 	
 	return 0;
 }
